137987e9bc11a1a069536113a1921f8a29c4b074
Alicia <23alicia.piyi@glimnet.se>
2025-03-19 11:27:15 +0200

    Simple footer design, cleared index

--- header.php
+++ header.php
@@ -17,6 +17,10 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="profile" href="https://gmpg.org/xfn/11">
 
+	<!-- För hämtning av symboler -->
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+
+	
 	<?php wp_head(); ?>
 </head>
 
