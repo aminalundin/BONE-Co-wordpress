139e3793c19ebe8898d86572efbc5bf8aafee1f4
aminalundin <lundinamina1@gmail.com>
2025-03-19 15:17:46 +0100

    styled header

--- header.php
+++ header.php
@@ -35,7 +35,6 @@
 			the_custom_logo();
 			if ( is_front_page() && is_home() ) :
 				?>
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
 				<?php
 			else :
 				?>
